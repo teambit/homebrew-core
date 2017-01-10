b0987b4c891a971f9b5bc0572d38679d172cf2c1
Amit <amit@cocycles.com>
2017-01-10 14:34:10 +0200

    added test to check if install works

--- Formula/bit.rb
+++ Formula/bit.rb
@@ -11,4 +11,8 @@ class Bit < Formula
     system "npm", "install", "-g", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
+
+  test do
+    system bin/"bit", "--version"
+  end
 end
